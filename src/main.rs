d569798d727bbfcf75ef57a22e056d8f14c67b7a
cvaize <cvaize@gmail.com>
2025-02-05 15:27:36 +0500

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+mod app;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .route("/home", web::get().to(app::controllers::web::home::index))
             .route("/hey", web::get().to(manual_hello))
     })
         .bind(("127.0.0.1", 8080))?
