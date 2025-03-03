15150abce1fc1ea422de6ec159b44914d40a145e
cvaize <cvaize@gmail.com>
2025-03-03 11:30:35 +0500

    Init

--- src/core/template.rs
+++ src/core/template.rs
@@ -6,9 +6,17 @@ pub fn new() -> TinyTemplate<'static> {
     tt.add_template("pages.error.default", RESOURCES_PAGES_ERROR_DEFAULT_HTML).unwrap();
     tt.add_template("pages.home.index", RESOURCES_PAGES_HOME_INDEX_HTML).unwrap();
     tt.add_template("pages.home.user", RESOURCES_PAGES_HOME_USER_HTML).unwrap();
+    tt.add_template("pages.auth.login", RESOURCES_PAGES_AUTH_LOGIN_HTML).unwrap();
+    tt.add_template("pages.auth.register", RESOURCES_PAGES_AUTH_REGISTER_HTML).unwrap();
+    tt.add_template("pages.auth.forgot-password", RESOURCES_PAGES_AUTH_FORGOT_PASSWORD_HTML).unwrap();
+    tt.add_template("pages.auth.forgot-password-confirm", RESOURCES_PAGES_AUTH_FORGOT_PASSWORD_CONFIRM_HTML).unwrap();
     tt
 }
 
 static RESOURCES_PAGES_ERROR_DEFAULT_HTML: &str = include_str!("../../resources/pages/error/default.html");
 static RESOURCES_PAGES_HOME_INDEX_HTML: &str = include_str!("../../resources/pages/home/index.html");
 static RESOURCES_PAGES_HOME_USER_HTML: &str = include_str!("../../resources/pages/home/user.html");
+static RESOURCES_PAGES_AUTH_LOGIN_HTML: &str = include_str!("../../resources/pages/auth/login.html");
+static RESOURCES_PAGES_AUTH_REGISTER_HTML: &str = include_str!("../../resources/pages/auth/register.html");
+static RESOURCES_PAGES_AUTH_FORGOT_PASSWORD_HTML: &str = include_str!("../../resources/pages/auth/forgot-password.html");
+static RESOURCES_PAGES_AUTH_FORGOT_PASSWORD_CONFIRM_HTML: &str = include_str!("../../resources/pages/auth/forgot-password-confirm.html");
--- src/routes/web.rs
+++ src/routes/web.rs
@@ -4,6 +4,10 @@ use crate::app::controllers;
 pub fn register(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/").route(web::get().to(controllers::web::home::index)));
     cfg.service(web::resource("/users").route(web::get().to(controllers::web::users::index)));
+    cfg.service(web::resource("/login").route(web::get().to(controllers::web::auth::login)));
+    cfg.service(web::resource("/register").route(web::get().to(controllers::web::auth::register)));
+    cfg.service(web::resource("/forgot-password").route(web::get().to(controllers::web::auth::forgot_password)));
+    cfg.service(web::resource("/forgot-password-confirm").route(web::get().to(controllers::web::auth::forgot_password_confirm)));
 
     // NotFound route
     cfg.service(web::scope("").wrap(controllers::web::errors::error_handlers()));
