89076dfb5650b146b915d4a52fd9fc5bc62e48ad
cvaize <cvaize@gmail.com>
2025-03-15 14:05:16 +0500

    Init

--- src/app/controllers/web/auth/login.rs
+++ src/app/controllers/web/auth/login.rs
@@ -1,16 +1,13 @@
-use std::ops::Deref;
-// use std::collections::HashMap;
-// use actix_session::Session;
 use crate::adapters::garde::GardeReportAdapter;
-use actix_web::{error, web, Error, HttpResponse, Result};
-use garde::external::compact_str::ToCompactString;
-use garde::{Report, Validate};
+use actix_session::Session;
+use actix_web::web::Redirect;
+use actix_web::{error, web, Error, HttpResponse, Responder, Result};
+use garde::Validate;
 use handlebars::Handlebars;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use serde_json::json;
-use serde_json::Value::Null;
-use crate::app::providers::template;
-// use crate::db_connection::DbPool;
+use std::collections::HashMap;
+use std::ops::Deref;
 
 #[derive(Validate, Deserialize, Debug)]
 pub struct SignInData {
@@ -20,41 +17,67 @@ pub struct SignInData {
     password: Option<String>,
 }
 
-pub async fn show(tmpl: web::Data<Handlebars<'_>>) -> Result<HttpResponse, Error> {
+#[derive(Serialize, Deserialize, Debug)]
+struct FlashData {
+    error: Option<String>,
+    errors: Option<HashMap<String, String>>,
+}
+
+impl FlashData {
+    pub fn empty() -> Self {
+        Self {
+            error: None,
+            errors: None,
+        }
+    }
+}
+
+static FLASH_DATA_KEY: &str = "app.sign_in.flash_data";
+
+pub async fn show(
+    session: Session,
+    tmpl: web::Data<Handlebars<'_>>,
+) -> Result<impl Responder, Error> {
+    // let mut tmpl: Handlebars = template::make();
+
+    let flash_data = session
+        .get::<String>(FLASH_DATA_KEY)
+        .unwrap_or(None)
+        .unwrap_or("{}".to_string());
+    let flash_data: FlashData = serde_json::from_str(&flash_data).unwrap_or(FlashData::empty());
+
     let ctx = json!({
-       "error" : &Null,
+        "title": "Вход",
+        "error" : &flash_data.error,
+        "errors": &flash_data.errors
     });
+
     let s = tmpl
         .render("pages/auth/login.hbs", &ctx)
-        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+        .map_err(|e| error::ErrorInternalServerError("Template error"))?;
+    session.remove(FLASH_DATA_KEY);
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 pub async fn sign_in(
-    tmpl: web::Data<Handlebars<'_>>,
+    session: Session,
     data: web::Form<SignInData>,
-) -> Result<HttpResponse, Error> {
-    // let mut tmpl: Handlebars = template::make();
-
-    let s = if let Err(report) = data.deref().validate() {
+) -> Result<impl Responder, Error> {
+    let ctx;
+    if let Err(report) = data.deref().validate() {
         let report_adapter = GardeReportAdapter::new(&report);
         let errors = report_adapter.to_hash_map();
-        let ctx = json!({
-            // "error" : "Ошибка".to_owned(),
-            "errors": errors
-        });
-        tmpl.render("pages/auth/login.hbs", &ctx)
-            .map_err(|e| {
-                dbg!(e);
-                return error::ErrorInternalServerError("Template error")
-            })?
-    } else {
-        let ctx = json!({
-           "error" : &Null,
-        });
-        tmpl.render("pages/auth/login.hbs", &ctx)
-            .map_err(|_| error::ErrorInternalServerError("Template error"))?
-    };
 
-    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+        ctx = FlashData {
+            error: Some("Ошибка валидации".to_string()),
+            errors: Some(errors),
+        };
+    } else {
+        ctx = FlashData::empty();
+        // TODO: Auth
+    };
+    let json = serde_json::to_string(&ctx)?;
+    session.insert(FLASH_DATA_KEY, json)?;
+
+    Ok(Redirect::to("/login").see_other())
 }
