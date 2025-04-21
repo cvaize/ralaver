f270969e27a6e2753338d4692ff21fbaa0e3f6f1
cvaize <cvaize@gmail.com>
2025-04-21 20:20:27 +0500

    Init

--- src/app/controllers/web/auth/logout.rs
+++ src/app/controllers/web/auth/logout.rs
@@ -1,41 +1,21 @@
+use crate::AuthService;
 use crate::{log_map_err, FlashService, Session};
-use crate::{Alert, AppService, AuthService, Translator, TranslatorService, ALERTS_KEY};
 use actix_web::web::Data;
 use actix_web::web::Redirect;
 use actix_web::{error, Error, HttpRequest, Responder, Result};
 
 pub async fn invoke(
-    req: HttpRequest,
     session: Session,
-    flash_service: Data<FlashService>,
     auth_service: Data<AuthService<'_>>,
-    app_service: Data<AppService>,
-    translator_service: Data<TranslatorService>,
 ) -> Result<impl Responder, Error> {
-    let flash_service = flash_service.get_ref();
     let auth_service = auth_service.get_ref();
-    let app_service = app_service.get_ref();
-    let translator_service = translator_service.get_ref();
 
-    let user = auth_service.authenticate_by_session(&session);
     auth_service
         .logout_from_session(&session)
         .map_err(log_map_err!(
             error::ErrorInternalServerError("AuthService error"),
             "Logout:invoke"
         ))?;
-
-    let (lang, _, _) = match user {
-        Ok(user) => app_service.locale(Some(&req), Some(&session), Some(&user)),
-        _ => app_service.locale(Some(&req), Some(&session), None),
-    };
-
-    let translator = Translator::new(&lang, translator_service);
-    let alert_str = translator.simple("auth.alert.logout.success");
-
-    let alerts = vec![Alert::success(alert_str)];
-
-    flash_service.save_throw_http(&session, ALERTS_KEY, &alerts)?;
 
     Ok(Redirect::to("/login").see_other())
 }
